688ca541c2a6ba952b38df25381489bab65f1c9e
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-08-02 20:20:46 -0300

    Add has_order method to model

--- store/shipping_addresses/models.py
+++ store/shipping_addresses/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
-from django.contrib.auth.models import User
-
-
+from users.models import User
 class ShippingAddress(models.Model):
     user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
     line1= models.CharField(max_length=50)
@@ -18,6 +16,9 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return self.postal_code
+
+    def has_orders(self):
+        return self.order_set.count() >= 1
 
     def update_default(self, default=False):
         self.default = default
