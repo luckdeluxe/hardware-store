0cf62336b08eab4536d788f670aa9a9da985b07b
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-08-02 20:24:26 -0300

    Update reverse url

--- store/orders/utils.py
+++ store/orders/utils.py
@@ -11,10 +11,10 @@ def get_or_create_order(request, cart):
         request.session['order_id'] = order.order_id
     return order
 
-def breadcrumb(products=True, addres=False, payment=False, confirmation=False):
+def breadcrumb(products=True, address=False, payment=False, confirmation=False):
     return [
         {'title':'Productos', 'active':products, 'url': reverse('orders:order')},
-        {'title':'Dirección', 'active':addres, 'url': reverse('orders:order')},
+        {'title':'Dirección', 'active':address, 'url': reverse('orders:address')},
         {'title':'Pago', 'active':payment, 'url': reverse('orders:order')},
         {'title':'Confirmación', 'active':confirmation, 'url': reverse('orders:order')},
     ]
