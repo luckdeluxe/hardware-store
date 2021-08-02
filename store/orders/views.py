3ef8553a4250c3cca4e0706b67bf389885ce4ae3
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-08-02 20:24:06 -0300

    Add select_address and adress method to views

--- store/orders/views.py
+++ store/orders/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from orders.utils import get_or_create_order, breadcrumb
 from carts.utils import get_or_create_cart
 from orders.models import Order
+from shipping_addresses.models import ShippingAddress
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='login')
@@ -14,3 +15,40 @@ def order(request):
         'breadcrumb': breadcrumb()
     })
 
+@login_required(login_url='login')
+def address(request):
+    cart = get_or_create_cart(request)
+    order = get_or_create_order(request, cart)
+    shipping_address = order.get_or_set_shipping_address()
+    can_choose_address = request.user.shippingaddress_set.count() > 1
+    
+    return render(request, 'orders/address.html', {
+        'cart': cart,
+        'order': order,
+        'shipping_address': shipping_address,
+        'breadcrumb': breadcrumb(address=True),
+        'can_choose_address': can_choose_address,
+    })
+
+@login_required(login_url='login')
+def select_address(request):
+    shipping_addresses = request.user.shippingaddress_set.all()
+    return render(request, 'orders/select_address.html', {
+        'breadcrumb': breadcrumb(address=True),
+        'shipping_addresses': shipping_addresses
+
+    })
+
+@login_required(login_url='login')
+def check_address(request, pk):
+    cart = get_or_create_cart(request)
+    order = get_or_create_order(request, cart)
+
+    shipping_address = get_object_or_404(ShippingAddress, pk=pk)
+
+    if request.user.id != shipping_address.user_id:
+        return redirect('carts:cart')
+
+    order.update_shipping_address(shipping_address)
+
+    return redirect('orders:address')
