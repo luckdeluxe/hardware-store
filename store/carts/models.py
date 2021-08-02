cf6ff7fd7a20520e448aafefe852cdf55a2c7ff2
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-08-02 20:31:30 -0300

    Update custom user imports

--- store/carts/models.py
+++ store/carts/models.py
@@ -1,7 +1,7 @@
 import uuid, decimal
 from django.db.models.signals import post_save, pre_save, m2m_changed
 from django.db import models
-from django.contrib.auth.models import User
+from users.models import User
 from django.db.models.fields import DecimalField
 from products.models import Product
 
