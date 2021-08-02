3bec5ee6602eddacafc1f207d89e0f7f7db95285
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-08-02 20:26:01 -0300

    Add address select and check path

--- store/orders/urls.py
+++ store/orders/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from orders.views import order
+from orders.views import address, check_address, order, select_address
 
 app_name = 'orders'
 
 urlpatterns = [
     path('', order, name='order'),
+    path('address', address, name='address'),
+    path('select/address', select_address, name='select_address'),
+    path('check/address/<int:pk>', check_address, name='check_address'),
 ]
