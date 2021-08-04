b10e413d3dbbc8dc8a672547a8c97e588353c566
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-08-04 09:10:07 -0300

    Add method of destruction and cancellation of order

--- store/orders/views.py
+++ store/orders/views.py
@@ -1,6 +1,7 @@
+from django.contrib import messages
 from django.shortcuts import get_object_or_404, redirect, render
-from orders.utils import get_or_create_order, breadcrumb
-from carts.utils import get_or_create_cart
+from orders.utils import destroy_order, get_or_create_order, breadcrumb
+from carts.utils import destroy_cart, get_or_create_cart
 from orders.models import Order
 from shipping_addresses.models import ShippingAddress
 from django.contrib.auth.decorators import login_required
@@ -52,3 +53,34 @@ def check_address(request, pk):
     order.update_shipping_address(shipping_address)
 
     return redirect('orders:address')
+
+@login_required(login_url='login')
+def confirm(request):
+    cart = get_or_create_cart(request)
+    order = get_or_create_order(request, cart)
+
+    shipping_address = order.shipping_address
+    if shipping_address is None:
+        return redirect('orders:address')
+
+    return render(request, 'orders/confirm.html', {
+        'cart': cart,
+        'order': order,
+        'shipping_address': shipping_address,
+        'breadcrumb': breadcrumb(address=True, confirmation=True)
+    })
+    
+@login_required(login_url='login')
+def cancel(request):
+    cart = get_or_create_cart(request)
+    order = get_or_create_order(request, cart)
+
+    if request.user.id != order.user_id:
+        return redirect('carts:cart')
+
+    order.cancel()
+    destroy_cart(request)
+    destroy_order(request)
+
+    messages.error(request, 'Orden cancelada')
+    return redirect('index')
