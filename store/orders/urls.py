2832fe088518621dd53dc2ec0c6342a4c4e148a5
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-08-04 09:11:35 -0300

    Add cancellation and confirmation path

--- store/orders/urls.py
+++ store/orders/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from orders.views import address, check_address, order, select_address
+from orders.views import address, cancel, check_address, confirm, order, select_address
 
 app_name = 'orders'
 
@@ -8,4 +8,6 @@ urlpatterns = [
     path('address', address, name='address'),
     path('select/address', select_address, name='select_address'),
     path('check/address/<int:pk>', check_address, name='check_address'),
+    path('confirm', confirm, name='confirm'),
+    path('cancel', cancel, name='cancel'),
 ]
