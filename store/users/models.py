fd1dc8ee4263d90c33b255e59b9dabcf8597ca4f
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-08-05 13:13:24 -0300

    Refactor queries in the model

--- store/users/models.py
+++ store/users/models.py
@@ -1,3 +1,4 @@
+from orders.common import OrderStatus
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -11,6 +12,16 @@ class User(AbstractUser):
 
     def has_shipping_address(self):
         return self.shipping_address is not None
+
+    def orders_completed(self):
+        return self.order_set.filter(status=OrderStatus.COMPLETED).order_by('-id')
+
+    def has_shipping_addresses(self):
+        return self.shippingaddress_set.exists()
+    
+    def addresses(self):
+        return self.shippingaddress_set.all()
+
 
 
 class Customer(User):
