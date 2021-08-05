ae20699ecb7999643d047d13f7b66e398d158b82
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-08-05 13:40:34 -0300

    Update confirm breadcrumb

--- store/orders/utils.py
+++ store/orders/utils.py
@@ -16,7 +16,7 @@ def breadcrumb(products=True, address=False, payment=False, confirmation=False):
         {'title':'Productos', 'active':products, 'url': reverse('orders:order')},
         {'title':'Dirección', 'active':address, 'url': reverse('orders:address')},
         {'title':'Pago', 'active':payment, 'url': reverse('orders:order')},
-        {'title':'Confirmación', 'active':confirmation, 'url': reverse('orders:order')},
+        {'title':'Confirmación', 'active':confirmation, 'url': reverse('orders:confirm')},
     ]
 
 def destroy_order(request):
