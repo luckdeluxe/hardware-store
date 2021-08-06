61cf7d4a8cfc0d24e8c42ed72a70e948c797f05d
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-08-06 07:19:18 -0300

    Add customer properties to user

--- store/users/models.py
+++ store/users/models.py
@@ -1,26 +1,54 @@
 from orders.common import OrderStatus
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from stripeAPI import customer
+from stripeAPI.customer import create_customer
 
 class User(AbstractUser):
+    customer_id = models.CharField(max_length=100, blank=True, null=True)
+
     def get_full_name(self):
-        return '{}-{}'.format(self.first_name, self.last_name)
+        return '{} {}'.format(self.first_name, self.last_name)
 
     @property
     def shipping_address(self):
         return self.shippingaddress_set.filter(default=True).first()
 
+    @property
+    def description(self):
+        return 'Descripcion para el usuario {}'.format(self.username)
+
+    def has_billing_profiles(self):
+        return self.billingprofile_set.exists()
+
+    def has_customer(self):
+        return self.customer_id is not None
+
+    def create_customer_id(self):
+        if not self.has_customer():
+            customer = create_customer(self)
+            self.customer_id = customer.id
+            self.save()
+    
     def has_shipping_address(self):
         return self.shipping_address is not None
 
     def orders_completed(self):
         return self.order_set.filter(status=OrderStatus.COMPLETED).order_by('-id')
 
+    @property
+    def billing_profile(self):
+        return self.billingprofile_set.filter(default=True).first()
+
     def has_shipping_addresses(self):
         return self.shippingaddress_set.exists()
     
     def addresses(self):
         return self.shippingaddress_set.all()
+
+    @property
+    def billing_profiles(self):
+        return self.billingprofile_set.all().order_by('-default')
 
 
 
