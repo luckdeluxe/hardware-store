e2bc5c94e350d65677d735d36285d281ff2a0049
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-08-06 07:33:34 -0300

    Update the payment address in the breadcrumb

--- store/orders/utils.py
+++ store/orders/utils.py
@@ -15,7 +15,7 @@ def breadcrumb(products=True, address=False, payment=False, confirmation=False):
     return [
         {'title':'Productos', 'active':products, 'url': reverse('orders:order')},
         {'title':'Dirección', 'active':address, 'url': reverse('orders:address')},
-        {'title':'Pago', 'active':payment, 'url': reverse('orders:order')},
+        {'title':'Pago', 'active':payment, 'url': reverse('orders:payment')},
         {'title':'Confirmación', 'active':confirmation, 'url': reverse('orders:confirm')},
     ]
 
